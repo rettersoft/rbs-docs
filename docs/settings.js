50995dff6404d84bfff9cb1e9dce52cd6f161f4e
yasaryavuzakbas <47295084+yasaryavuzakbas@users.noreply.github.com>
2022-03-23 16:20:54 +0300

    3

---
+++ docs/settings.js
@@ -0,0 +1,7 @@
+"rest-client.environmentVariables": {
+    "admin": {
+        "host": "core-test.rettermobile.com",
+        "projectId": "933a51e1c87a9ccc181d21fca91c2aad",
+        "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6InJicy5idXNpbmVzc3VzZXJhdXRoLmFkbWluIiwiYW5vbnltb3VzIjpmYWxzZSwicHJvamVjdElkIjoiOTMzYTUxZTFjODdhOWNjYzE4MWQyMWZjYTkxYzJhYWQiLCJ1c2VySWQiOiJlbWFpbEB0ZXN0LmNvbSIsInRpbWVzdGFtcCI6MTYwODEyOTI2NzI5OSwiaWF0IjoxNjA4MTI5MjY3LCJleHAiOjE2MTgxMjkyNjd9.KfXN9CINUMG8vGNeVEAKBINwQ9FQLyLCcL7aKhdTSwY"
+    }
+}
